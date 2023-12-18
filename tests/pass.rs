34457cb7223e11f6d12d7593f6a12d0ea0721185
Emil Ørup Kristensen <eokr@itu.dk>
2023-12-18 17:14:21 +0100

    erl to core translation check

--- tests/pass.rs
+++ tests/pass.rs
@@ -10,8 +10,30 @@ use test_generator::test_resources; // TODO: Small lib, maybe use something else
 use std::process::Command;
 
 // TODO Verify .core file is correctly translated from .erl
-// #[test_resources("tests/pass/*.erl")]
-// fn erl_core_consistentcy
+#[test_resources("tests/pass/*.erl")]
+fn erl_core_consistency(resource: &str) {
+    let mut core_file: String = resource[0..resource.len()-4].to_string();
+    core_file.push_str(".core");
+
+    match std::fs::read_to_string(core_file.clone()) {
+        Ok(original) => {
+            
+            let status = Command::new("erlc")
+                .arg("+to_core")
+                .arg("-o")
+                .arg("tests/pass")
+                .arg(resource)
+                .status()
+                .expect("");
+            assert_eq!(0, status.code().unwrap());
+            match std::fs::read_to_string(core_file) { // Read again
+                Ok(freshly_compiled) => assert_eq!(original,freshly_compiled),
+                Err(err) => panic!("Could not read file {}", err),
+            }
+        },
+        Err(err) => panic!("Could not read file {}", err),
+    }
+}
 
 // TODO Verify that erlc accepts all *.core files
 #[test_resources("tests/pass/*.core")]
