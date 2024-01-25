d7bed1385d34d957ddda6c970b18dfd713b89ea9
Emil Ørup Kristensen <eokr@itu.dk>
2024-01-25 20:32:53 +0100

    fun cal fun starting example

---
+++ tests/analysis/pass/fun-to-fun.erl
@@ -0,0 +1,26 @@
+-module('fun-to-fun').
+
+% Function calling another function to consume part of session type
+% By Emil Kristensen, ITU 2024
+
+% Public functions in this module
+-export([foo/1, bar/3]).
+
+-type new() :: {}. % changed from server() as constructor
+-type ongoing() :: {}. % changed from session()
+
+% TODO: Bad: Syntax does not allow custom tags after function definitions! Maybe we should have used comments instead?
+% Though I would have ad to use erlang directly then.
+-session("'foo'(new(!number. ?string. !string. ?number.  end.),_) -> _, [ SessionID:  end. ]  ").
+-session("'bar'(_,ongoing(!string. ?number. -> end.),_) -> _, []").
+
+-spec foo(new()) -> number().
+foo(ServerPid) ->
+    SessionID = gen_server_plus:call(ServerPid,new), 
+    Res = gen_server_plus:call(ServerPid,SessionID,42),
+
+    bar(ServerPid, SessionID, Res).
+
+-spec bar(pid(),ongoing(),string()) -> number().
+bar(ServerPid, SessionID, Str) ->
+    gen_server_plus:call(ServerPid,SessionID,Str).
