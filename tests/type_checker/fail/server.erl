187729f5cb788c268a64be84b5373cade4842cca
Emil Kristensen <eKristensen@users.noreply.github.com>
2024-09-02 20:32:46 +0200

    Tests works again
    Signed-off-by: Emil Kristensen <eKristensen@users.noreply.github.com>

--- tests/type_checker/fail/server.erl
+++ tests/type_checker/fail/server.erl
@@ -1,7 +1,7 @@
 -module(server).
 
 % elp:ignore W0013 (misspelled_attribute)
--mspec('?neg !ready ?int !int.').
+-mspec("?'neg'. !'ready'. ?integer. !integer.").
 
 % A simple static dual check
 % By Emil Kristensen, ITU 2024
